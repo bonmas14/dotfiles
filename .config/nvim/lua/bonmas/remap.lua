48137a19c32273816dce99d0d39f2fdc34aff89b
bonmas14 <bonmas14@gmail.com>
2024-07-26 21:00:07 +0300

    Vex Ex and some terminal stuff

--- .config/nvim/lua/bonmas/remap.lua
+++ .config/nvim/lua/bonmas/remap.lua
@@ -1,5 +1,9 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", ":Ex<Enter>")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Vex)
+vim.keymap.set("n", "<leader>pe", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>t", "<C-w>v<C-w>w:term<Enter>i")
+vim.keymap.set("t", "<ESC><ESC>", "<C-\\><C-n>:q<Enter>")
+vim.keymap.set("t", "<C-w>", "<C-\\><C-n>")
 
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
@@ -8,4 +12,4 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
-vim.keymap.set("n", "<leader>gr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
