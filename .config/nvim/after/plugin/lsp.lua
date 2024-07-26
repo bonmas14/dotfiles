be2b54fc8284a45d705fc7acd4cfbf5a3f343dd6
bonmas14 <bonmas14@gmail.com>
2024-07-26 21:10:57 +0300

    deleted omnisharp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -6,15 +6,4 @@ end)
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
-
-require'lspconfig'.omnisharp.setup {
-    enable_editorconfig_support = true,
-    enable_ms_build_load_projects_on_demand = false,
-    enable_roslyn_analyzers = false,
-    organize_imports_on_format = false,
-    enable_import_completion = false,
-    sdk_include_prereleases = true,
-    analyze_open_documents_only = false,
-}
-
 lsp.setup()
