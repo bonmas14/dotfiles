020f220f9ac682c0c6dfe111ca118d1590d80c6a
bonmas14 <bonmas14@gmail.com>
2024-07-26 21:09:44 +0300

    Harpoon normal keybindings

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -1,10 +1,13 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<Leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<Leader>h<Leader>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<Leader>ha", mark.add_file)
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<Leader>hw", function() ui.nav_next() end)
+vim.keymap.set("n", "<Leader>hs", function() ui.nav_prew() end)
+
+vim.keymap.set("n", "<Leader>hj", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<Leader>hk", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<Leader>hl", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<Leader>h;", function() ui.nav_file(4) end)
