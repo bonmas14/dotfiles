39575ba42984f9e43a4a685d3147c5d3ca7c5507
bonmas14 <bonmas14@gmail.com>
2025-06-03 00:25:31 +0300

    deleted unnecessary comment

--- install_nvim.bat
+++ install_nvim.bat
@@ -1,5 +1,7 @@
 @echo off
 
-:: git clone https://github.com/wbthomason/packer.nvim %LOCALAPPDATA%\nvim-data\site\pack\packer\start\packer.nvim
+if not exist %LOCALAPPDATA%\nvim (
+    mkdir %LOCALAPPDATA%\nvim
+)
 
 xcopy /E /y .\nvim %LOCALAPPDATA%\nvim
