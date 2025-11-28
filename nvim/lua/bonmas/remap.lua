0f5c67c238cd7921648f53fb3b8b83d40b7c679e
bonmas14 <bonmas14@gmail.com>
2025-11-28 15:39:01 +0300

    Keybind for my build files

--- nvim/lua/bonmas/remap.lua
+++ nvim/lua/bonmas/remap.lua
@@ -21,3 +21,4 @@ vim.keymap.set("n", "<leader>h",  ":tabprevious<Enter>")
 vim.keymap.set("n", "<leader>s", [[:s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>r", [[n:~<Enter>]])
 
+vim.keymap.set("n", "<leader>b", [[:tab :te build]])
